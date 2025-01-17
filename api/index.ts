b499b8d4cf6233d24430185aabb6643c28b23106
e24maliv <ervan.malivet@imt-atlantique.net>
2025-01-17 11:37:47 +0100

    Init typescript

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000
+const port = 3000;
 
 app.get('/', (req, res) => {
 
@@ -13,5 +13,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port', port);
 });
